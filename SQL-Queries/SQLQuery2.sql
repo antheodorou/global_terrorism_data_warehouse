ac724fdae3bf1485551be55abaeb78301b72fb19
Anastasios Theodorou <32821928+antheodorou@users.noreply.github.com>
2022-01-09 12:51:21 +0200

    Delete SQLQuery2.sql

--- SQL-Queries/SQLQuery2.sql
+++
@@ -1,6 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [country]
-      ,[country_txt]
-  FROM [ter_db].[dbo].[Countries]
-
-  delete from [ter_db].[dbo].[Countries] where country = 0
