aba24ac38ec61422f9940b6537ae347ccecb915d
Anastasios Theodorou <32821928+antheodorou@users.noreply.github.com>
2022-01-09 12:51:32 +0200

    Delete SQL_server.sql

--- SQL-Queries/SQL_server.sql
+++
@@ -1,8 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [crit1_dim]
-      ,[label]
-  FROM [restoreTer].[dbo].[crit1_dimension]
-
-  
-
-    SELECT @@SERVERNAME
